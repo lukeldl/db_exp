fabb7e0c8426d6a3fd05581d7e813a4cd80e2508
lukeldl <68222964+lukeldl@users.noreply.github.com>
2021-11-01 04:50:55 +0800

    Add files via upload

---
+++ src/main/java/org/scu/db/demo/mapper/MemberMapper.java
@@ -0,0 +1,12 @@
+package org.scu.db.demo.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.scu.db.demo.model.Member;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface MemberMapper {
+
+}
---
+++ src/main/java/org/scu/db/demo/service/impl/TitleServiceImpl.java
@@ -0,0 +1,15 @@
+package org.scu.db.demo.service.impl;
+
+import org.scu.db.demo.mapper.TitleMapper;
+import org.scu.db.demo.model.Title;
+import org.scu.db.demo.service.TitleService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class TitleServiceImpl implements TitleService {
+    @Autowired
+    private TitleMapper titleMapper;
+    public List<Title> findIliadOrOdysseyTitle(){return titleMapper.findIliadOrOdysseyTitle();}
+
+}
